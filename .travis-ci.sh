c3772bab94b8ff66ae33769e424b75bc5280163d
Anil Madhavapeddy <anil@recoil.org>
2013-09-28 13:04:26 +0100

    Travis: install libxen-dev extdep

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -13,6 +13,10 @@ esac
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+
+# TODO add this to the OPAM extdep
+sudo apt-get install -qq libxen-dev
+
 export OPAMYES=1
 export OPAMVERBOSE=1
 echo OCaml version
