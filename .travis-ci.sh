a8ac40d61dc1385a9c3cc0a1be961eeb2cf36743
David Scott <dave.scott@citrix.com>
2014-06-16 22:54:34 +0100

    travis: install mirage-types instead of mirage
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-OPAM_DEPENDS="lwt cstruct mirage xenstore xenstore_transport io-page xen-evtchn xen-gnt mirage cmdliner"
+OPAM_DEPENDS="lwt cstruct mirage-types xenstore xenstore_transport io-page xen-evtchn xen-gnt cmdliner"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
