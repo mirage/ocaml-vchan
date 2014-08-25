0097501ca00aa31cfa41c3563b8e1e3719e4d412
David Scott <dave.scott@citrix.com>
2014-08-25 21:12:18 +0000

    xencat: use the Vchan_lwt_unix (Lwt_io channel)-based interface for the client
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- cli/xencat.ml
+++ cli/xencat.ml
@@ -1,22 +1,12 @@
 open Cmdliner
 open Sexplib.Std
+open Vchan_lwt_unix
 
 type clisrv = Client | Server
 
 let (>>=) = Lwt.bind
 
 module Xs = Xs_client_lwt.Client(Xs_transport_lwt_unix_client)
-module V = Vchan_lwt_unix.M
-
-let with_vchan clisrv evtchn_h domid nodepath f =
-  (match clisrv with
-   | Client ->
-     V.client ~evtchn_h ~domid ~xs_path:nodepath
-   | Server ->
-     V.server ~evtchn_h ~domid ~xs_path:nodepath
-       ~read_size:5000 ~write_size:5000 ~persist:true)
-  >>= fun vch ->
-  f vch
 
 let listen =
   let doc = "Act as a server rather than a client." in
@@ -31,12 +21,12 @@ let buf = String.create 5000
 let (>>|=) m f = m >>= function
 | `Ok x -> f x
 | `Eof -> Lwt.fail (Failure "End of file")
-| `Error (`Not_connected state) -> Lwt.fail (Failure (Printf.sprintf "Not in a connected state: %s" (Sexplib.Sexp.to_string (V.sexp_of_state state))))
+| `Error (`Not_connected state) -> Lwt.fail (Failure (Printf.sprintf "Not in a connected state: %s" (Sexplib.Sexp.to_string (M.sexp_of_state state))))
 
 let with_vchan_f vch =
   let (_: unit Lwt.t) =
     let rec read_forever vch =
-      V.read vch >>|= fun buf ->
+      M.read vch >>|= fun buf ->
       Printf.printf "%s%!" (Cstruct.to_string buf);
       read_forever vch in
     read_forever vch in
@@ -47,16 +37,41 @@ let with_vchan_f vch =
       let line = line ^ "\n" in
       let buf = Cstruct.create (String.length line) in
       Cstruct.blit_from_string line 0 buf 0 (String.length line);
-      V.write vch buf
+      M.write vch buf
       >>|= fun () ->
       stdin_to_endpoint vch in
     stdin_to_endpoint vch in
   let t, u = Lwt.task () in
   t
 
+let with_vchan clisrv evtchn_h domid nodepath f =
+  (match clisrv with
+   | Client ->
+     M.client ~evtchn_h ~domid ~xs_path:nodepath
+   | Server ->
+     M.server ~evtchn_h ~domid ~xs_path:nodepath
+       ~read_size:5000 ~write_size:5000 ~persist:true)
+  >>= fun vch ->
+  f vch
+
+let client domid nodepath =
+  Client.connect ~domid ~path:nodepath
+  >>= fun (ic, oc) ->
+  let rec proxy a b =
+    Lwt_io.read_char a
+    >>= fun c ->
+    Lwt_io.write_char b c
+    >>= fun () ->
+    proxy a b in
+  let (a: unit Lwt.t) = proxy Lwt_io.stdin oc in
+  let (b: unit Lwt.t) = proxy ic Lwt_io.stdout in
+  Lwt.join [a; b]
+  >>= fun () ->
+  Client.close (ic, oc)
+
 open Lwt
 
-let node listen domid nodepath : unit Lwt.t = Lwt_main.run (
+let node listen domid nodepath : unit = Lwt_main.run (
   ( match nodepath with
     | Some s -> return s
     | None ->
@@ -69,8 +84,10 @@ let node listen domid nodepath : unit Lwt.t = Lwt_main.run (
   >>= fun nodepath ->
   (* Listen to incoming events. *)
   let evtchn_h = Eventchn.init () in
-  let clisrv = if listen then Server else Client in
-  with_vchan clisrv evtchn_h domid nodepath with_vchan_f)
+  if listen
+  then with_vchan Server evtchn_h domid nodepath with_vchan_f
+  else client domid nodepath
+)
 
 let cmd =
   let doc = "Establish vchan connections" in
