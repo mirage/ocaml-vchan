e55a394054d36f9f7d5ad582fdcbe49ccded2cbc
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-09-17 15:53:54 +0000

    Install uuid-dev in travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -15,7 +15,7 @@ sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 
 # TODO add this to the OPAM extdep
-sudo apt-get install -qq libxen-dev
+sudo apt-get install -qq libxen-dev uuid-dev
 
 export OPAMYES=1
 export OPAMVERBOSE=1
