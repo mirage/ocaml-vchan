138d1849120e3a14d3675623c7eddb47aa8f80f7
David Scott <dave.scott@citrix.com>
2014-11-29 13:23:51 +0000

    Add a cleaner "in-memory" events implementation
    This replaces the Xen-like array of event channels with individual
    records in the heap, each of which has a counter and a condition
    variable.
    
    Eventually this module should probably be moved to xen-evtchn.
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- lib/in_memory.ml
+++ lib/in_memory.ml
@@ -154,71 +154,9 @@ module Memory = struct
     end
 end
 
-module Events = struct
-  open Lwt
-
-  type port = int with sexp_of
-
-  let port_of_string x = `Ok (int_of_string x)
-  let string_of_port = string_of_int
-
-  type channel = int with sexp_of
-  let get =
-    let g = ref 0 in
-    fun () ->
-      incr g;
-      !g - 1
-
-  type event = int with sexp_of
-  let initial = 0
-
-  let channels = Array.make 1024 0
-  let c = Lwt_condition.create ()
-
-  let rec recv channel event =
-    if channels.(channel) > event
-    then return channels.(channel)
-    else
-      Lwt_condition.wait c >>= fun () ->
-      recv channel event
-
-  let connected_to = Array.make 1024 (-1)
-
-  let send channel =
-    let listening = connected_to.(channel) in
-    if listening = -1 then begin
-      Printf.fprintf stderr "send: event channel %d is closed\n%!" channel;
-      failwith (Printf.sprintf "send: event channel %d is closed" channel);
-    end;
-    channels.(listening) <- channels.(listening) + 1;
-    Lwt_condition.broadcast c ()
-
-  let listen _ =
-    let port = get () in
-    port, port
-
-  let connect _ port =
-    let port' = get () in
-    connected_to.(port') <- port;
-    connected_to.(port) <- port';
-    port'
-
-  let close port =
-    channels.(port) <- 0;
-    connected_to.(port) <- -1
-
-  let assert_cleaned_up () =
-    for i = 0 to Array.length connected_to - 1 do
-      if connected_to.(i) <> (-1) then begin
-        Printf.fprintf stderr "Some event channels are still connected\n%!";
-        failwith "some event channels are still connected"
-      end
-    done
-end
-
 let assert_cleaned_up () =
   Memory.assert_cleaned_up ();
   Config.assert_cleaned_up ();
-  Events.assert_cleaned_up ()
+  In_memory_events.assert_cleaned_up ()
 
-include Endpoint.Make(Events)(Memory)(Config)
+include Endpoint.Make(In_memory_events)(Memory)(Config)
