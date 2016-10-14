9a4e823009284ab53ba7e2ac0bc5fb2a7be8799a
Mindy Preston <mindy.preston@docker.com>
2016-10-14 04:18:55 -0500

    use -t xen instead of --xen for mirage configure

--- lib_test/mirage/build.sh
+++ lib_test/mirage/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-mirage configure -f config_server.ml --xen --no-opam
+mirage configure -f config_server.ml -t xen --no-opam
 mirage build -f config_server.ml
-mirage configure -f config_client.ml --xen --no-opam
+mirage configure -f config_client.ml -t xen --no-opam
 mirage build -f config_client.ml
