6205f54f429f3a5c3227c839543d7efaf6d9e918
Mike Neilsen <mneilsen@acm.org>
2017-09-06 15:34:54 -0500

    Update lib_test/mirage for Mirage 3.x
    https://mirage.io/wiki/mirage2-to-mirage3 was an incredible resource for
    helping me update this test code.  Tested against Mirage 3.0.5 and
    ocaml-vchan 3.0.0.

--- lib_test/mirage/build.sh
+++ lib_test/mirage/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-mirage configure -f config_server.ml -t xen --no-opam
+mirage configure -f config_server.ml -t xen
 mirage build -f config_server.ml
-mirage configure -f config_client.ml -t xen --no-opam
+mirage configure -f config_client.ml -t xen
 mirage build -f config_client.ml
